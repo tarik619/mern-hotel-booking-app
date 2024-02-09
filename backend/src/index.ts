12d16bdadbba55700c8349018e1ba5bdc38ea32d
tarik619 <itarik707@gmail.com>
2024-02-10 00:08:22 +0600

    setting backend for typescript installed some dependencies

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
