330a3d68848b1a9c690f3ed289483084aab004cc
tarik619 <itarik707@gmail.com>
2024-02-10 11:20:24 +0600

    creating user model and make a route for register a user and handling errors with express-validator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route";
 
 mongoose
   .connect(process.env.MONGO_URI as string)
@@ -16,6 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ backend/src/routes/user.route.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express";
+import User from "../models/user.model";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    check("firstName", "First name is required").isString(),
+    check("lastName", "Last name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").isLength({ min: 6 }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        res.status(400).json({ message: "USer already exist" });
+      }
+      user = new User(req.body);
+      await user.save();
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET as string
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+      return res.sendStatus(200);
+    } catch (error) {
+      res.status(500).json({ message: "Server Error" });
+      console.log(error);
+    }
+  }
+);
+
+export default router;
